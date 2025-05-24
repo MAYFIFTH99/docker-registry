0b452996c06bd77a8fd882166361e68d8c0cd7e2
harvey <alstjr971@naver.com>
2025-05-24 17:43:52 +0900

    chore: first commit

---
+++ src/main/java/opensource/dockerregistry/DockerRegistryApplication.java
@@ -0,0 +1,13 @@
+package opensource.dockerregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerRegistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/opensource/dockerregistry/DockerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package opensource.dockerregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
