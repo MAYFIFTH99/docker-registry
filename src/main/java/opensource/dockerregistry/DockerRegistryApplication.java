8aa23a1172e9c383a5d6fae3268b7d59b20cb77d
harvey <alstjr971@naver.com>
2025-05-31 20:03:13 +0900

    feat: SpringSecurity Authorization 추가 및 BCryptor 빈 위치 변경

--- src/main/java/opensource/dockerregistry/DockerRegistryApplication.java
+++ src/main/java/opensource/dockerregistry/DockerRegistryApplication.java
@@ -3,9 +3,6 @@ package opensource.dockerregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DockerRegistryApplication {
@@ -14,8 +11,4 @@ public class DockerRegistryApplication {
         SpringApplication.run(DockerRegistryApplication.class, args);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
